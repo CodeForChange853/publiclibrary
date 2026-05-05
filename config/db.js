06331403e8f0826bb4da4312e3417aaa3db85a4c
CodeForChange853 <garciaadrian1030@gmail.com>
2026-05-05 08:12:21 +0800

    db js change

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ require('dotenv').config(); // Load password from .env file
 // Connection pool
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306, // <-- ADD THIS LINE FOR AIVEN
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME || 'plals_db',
