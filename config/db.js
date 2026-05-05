d6ed20dd0536c3f5a748a91559158dd771e79a69
CodeForChange853 <garciaadrian1030@gmail.com>
2026-05-05 11:29:21 +0800

    fix

--- config/db.js
+++ config/db.js
@@ -4,13 +4,17 @@ require('dotenv').config(); // Load password from .env file
 // Connection pool
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 3306, // <-- ADD THIS LINE FOR AIVEN
+  port: process.env.DB_PORT || 23284,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || 'plals_db',
+  database: process.env.DB_NAME || 'defaultdb',
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  // <--- THIS IS THE MISSING PIECE FOR AIVEN --->
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool.promise();
